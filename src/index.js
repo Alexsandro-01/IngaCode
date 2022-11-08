7fcc1cbaea8e466096293a7423cb5bb9b48bd188
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-08 08:31:13 -0300

    style: :recycle: Correções do eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const app = require('./server');
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listen at port ${PORT}`));
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  res.status(200).json({ message: 'Hello!'});
+  res.status(200).json({ message: 'Hello!' });
 });
 
 module.exports = app;
