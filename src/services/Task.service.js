3dd5260a1c784b4f4e37c798fd8bae5fc1949591
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-13 19:06:53 -0300

    feat: :sparkles: Service para retornar todas as tasks e seus timetrackers

--- src/services/Task.service.js
+++ src/services/Task.service.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const { v4: uuidv4 } = require('uuid');
 const taskSchema = require('../validations/task');
 const updateTaskSchema = require('../validations/updateTask');
@@ -8,6 +9,9 @@ const TaskModel = require('../models/Tasks.model');
 const existsProjectById = require('./existsprojectById');
 const authUser = require('./authUserByToken');
 const getTaskById = require('./getTaskById');
+const { getAllTimeTrackersOnDB } = require('./TimeTracker.service');
+const getAllCollaboratorsOnDB = require('./getAllCollaboratorsOnDB');
+const getAllProjectsOnDB = require('./getAllProjectsOnDB');
 
 async function createNewTaskOnDB(payload) {
   try {
@@ -33,6 +37,24 @@ async function updateTaskById(payload, _id) {
         UpdatedAt: new Date().toJSON(),
       },
     );
+  } catch (error) {
+    Errors.InternalServerError();
+  }
+}
+
+async function getAlltasksOnDB() {
+  try {
+    const response = await TaskModel.find(
+{
+      where: { DeletedAt: null },
+    },
+    {
+      DeletedAt: 0,
+      UpdatedAt: 0,
+      CreatedAt: 0,
+    },
+);
+    return response;
   } catch (error) {
     Errors.InternalServerError();
   }
@@ -90,7 +112,45 @@ async function updateTaskService(payload, taskId, token) {
   await updateTaskById(parsedTask.data, taskId);
 }
 
+function joinTables(trackers, tasks, collaborators, projects) {
+  const response = trackers.map((tracker) => {
+    const collaborator = collaborators.filter((value) => value._id === tracker.CollaboratorId);
+
+    const task = tasks.filter((value) => value._id === tracker.TaskId);
+
+    projects.forEach((project) => {
+      const a = project._id;
+      const b = task[0].ProjectId;
+      if (a === b) {
+        task[0]._doc.ProjectName = project.Name;
+      }
+    });
+
+    return {
+      ...tracker.toJSON(),
+      Collaborator: collaborator[0].Name,
+      Task: task,
+    };
+  });
+  
+  return response;
+}
+
+async function getTasksService(token) {
+  await authUser(token);
+
+  const trackers = await getAllTimeTrackersOnDB();
+  const tasks = await getAlltasksOnDB();
+  const collaborators = await getAllCollaboratorsOnDB();
+  const projects = await getAllProjectsOnDB();
+  
+  const response = joinTables(trackers, tasks, collaborators, projects);
+
+  return response;
+}
+
 module.exports = {
   createTaskService,
   updateTaskService,
+  getTasksService,
 };
