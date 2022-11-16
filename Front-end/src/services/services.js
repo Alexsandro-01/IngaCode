c494561a4416236f9ef9e6ce682862fad8a11103
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-11-16 20:36:42 -0300

    feat: :sparkles: Adiciona novas funções para requisições http para a API

--- Front-end/src/services/services.js
+++ Front-end/src/services/services.js
@@ -17,6 +17,24 @@ export async function requestLogin(data) {
   return user;
 };
 
+export async function requestCollaborators(token) {
+  const url = 'http://localhost:3001/collaborators'
+
+  const obj = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      authorization: token,
+    }
+  }
+
+  const response = await fetch(url, obj);
+
+  const collaborators = await response.json()
+  return collaborators;
+}
+
 export async function requestCreateProject(data, token) {
   const url = 'http://localhost:3001/projects/create';
 
@@ -128,5 +146,122 @@ export async function requestCreateTask(data, token) {
   const response = await fetch(url, obj);
   
   return response;
+};
+
+export async function requestUpdateTask(data, taskId, token) {
+  const url = `http://localhost:3001/tasks/update/${taskId}`;
+
+  const dataJson = JSON.stringify(data);
+
+  const obj = {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: dataJson
+  }
+  
+  const response = await fetch(url, obj);
+  
+  return response;
+};
+
+export async function requestDeleteTask(taskId, token) {
+  const url = `http://localhost:3001/tasks/delete/${taskId}`;
+
+  const obj = {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  }
+  
+  const response = await fetch(url, obj);
+  
+  return response;
+};
+
+export async function requestCreateTimeTracker(data, token) {
+  const url = 'http://localhost:3001/timetrackers/create';
+
+  const dataJson = JSON.stringify(data);
+
+  const obj = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: dataJson
+  }
+  
+  const response = await fetch(url, obj);
+  
+  return response;
+};
+
+export async function requestUpdateTimeTracker(
+  data,
+  timetrackerId,
+  token
+  ) {
+  const url = `http://localhost:3001/timetrackers/update/${timetrackerId}`;
+
+  const dataJson = JSON.stringify(data);
+
+  const obj = {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    body: dataJson
+  }
+  
+  const response = await fetch(url, obj);
+  
+  return response;
+};
+
+export async function requestDeleteTimeTracker(timetrackerId, token) {
+  const url = `http://localhost:3001/timetrackers/delete/${timetrackerId}`;
+
+  const obj = {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  }
+  
+  const response = await fetch(url, obj);
+  
+  return response;
+};
+
+export async function requestTime(token) {
+  const url = `http://localhost:3001/timetrackers/get-time`;
+
+  const obj = {
+    method: 'get',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  }
+  
+  const response = await fetch(url, obj);
+
+  const time = await response.json()
+  
+  return time;
 };
 
